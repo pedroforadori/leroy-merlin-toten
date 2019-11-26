d086e42e0ded72429650148f82372d8c01663968
Batuta-10 <gabriel.costa@fcamara.com.br>
2019-11-26 18:55:38 -0300

    fix Spaces and structure

--- src/pages/products/style.js
+++ src/pages/products/style.js
@@ -4,36 +4,42 @@ import theme from '../../styles/theme'
 
 export const Container = styled.div`
   display: grid;
-  grid-gap: 50px;
+  grid-column-gap: 22px;
+  grid-row-gap: 45px;
   grid-template-columns: repeat(auto-fit, 239px);
-  justify-content: center;
-  padding: 100px 50px;
+  justify-content: space-between;
+  padding: 0 47px 80px 47px;
 `
 
 export const Category = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  background-image: url(${({ src }) => src});
-  background-position: 50% 15%;
-  background-repeat: no-repeat;
+  align-items:center;
   height: 432px;
   width: 239px;
-  border: 1px solid #666666;
+  border: 1px solid ${theme.darkGray};
   box-sizing: border-box;
-  padding-top: 10px;
+  padding: 10px;
+`
 
-  div {
-    margin-left: 10px;
-  }
+export const BoxDecription = styled.div`
+  display:flex;
+  flex-direction:column;
+  width:100%;
 `
 
 export const DescriptionProduct = styled.div`
   font-size: 16px;
   line-height: 19px;
-  padding-top: 250px;
+  margin-top: 14px;
   /* cinza */
   color: #666666;
+  min-height:57px;
+  
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
 `
 
 export const IdProduct = styled.div`
@@ -41,17 +47,33 @@ export const IdProduct = styled.div`
   opacity: 0.6;
   font-size: 12px;
   line-height: 144%;
-  margin-top: 5px;
+  margin-top: 6px;
 `
 
 export const PriceProduct = styled.div`
   font-style: normal;
   font-weight: normal;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 19px;
   /* cinza */
   color: #333333;
-  margin-top: 10px;
+  margin-top: 15px;
+  position:relative;
+
+  span {
+    font-size: 24px;
+  }
+
+  span:nth-of-type(2) {
+    position:absolute;
+    font-size: 12px;
+    top:-5px;
+  }
+
+  span:nth-of-type(3) {
+    font-size: 14px;
+    margin-left:20px;
+  }
 `
 
 export const PortionProduct = styled.div`
